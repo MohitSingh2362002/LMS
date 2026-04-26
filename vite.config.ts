fe7f2123ebf254078823f0763b157c95c7fa9e3c
MohitSingh2362002 <mohitsingh2362002@gmail.com>
2026-04-26 15:05:14 +0530

    akn

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [react(), basicSsl()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    ...(mode === 'development' ? [basicSsl()] : []),
+  ],
   server: {
     port: 3000,
     host: true,
   },
-})
+}))
